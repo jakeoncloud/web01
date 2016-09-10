183c2d623910f820cc4f244b6e682f5511f814f5
James Liu <jinkishere@live.com>
2016-09-10 17:18:37 +0800

    create a simple view for rango returning a string.

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(requeset):
+    return HttpResponse('Rango says hey there partner!')
