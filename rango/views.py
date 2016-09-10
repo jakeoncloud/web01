2fa9cb0c5f92268ad2920c22e73cb06c82e27143
James Liu <jinkishere@live.com>
2016-09-10 21:13:52 +0800

    Update index view of rango - now that index view is using the template engine - index now renders a boldmessage template variable.

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,13 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Rango says hey there partner! <br /> <a href='/rango/about/'> About Rango </a>")
+    # Construct a dictionary to pass to template engine as its context.
+    context_dict = {"boldmessage": "This is BOLD!"}
+    # Render the tamplate with request and the template context.
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <br /> <a href='/rango/'> Home of Rango </a>")
